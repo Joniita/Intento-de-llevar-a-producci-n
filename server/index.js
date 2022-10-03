3e6685058211461c43bba05156e1aebfeac0cae4
Joniita <joniita.d@gmail.com>
2022-10-03 01:20:23 -0300

    configurando el PORT

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "../client/config.js";
+import { PORT } from "./config.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
